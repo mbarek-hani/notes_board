d1c67fefd8b3b6f3d758e8b9d28a8baab201dae1
mbarek-hani <mbarekhani30@gmail.com>
2026-02-14 13:51:17 +0100

    add react toastify to show messages to the user instead of logging them to the console

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,23 +2,32 @@ import { Routes, Route } from "react-router-dom";
 import Notes from "./pages/Notes";
 import NotFound from "./pages/NotFound";
 import NotesProvider from "./contexts/NotesContext";
+import { ToastContainer } from "react-toastify";
 
 function App() {
-	return (
-		<div id="app">
-			<Routes>
-				<Route
-					path="/notes"
-					element={
-						<NotesProvider>
-							<Notes />
-						</NotesProvider>
-					}
-				/>
-				<Route path="*" element={<NotFound />} />
-			</Routes>
-		</div>
-	);
+  return (
+    <div id="app">
+      <Routes>
+        <Route
+          path="/notes"
+          element={
+            <NotesProvider>
+              <Notes />
+            </NotesProvider>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <ToastContainer
+        position="top-right"
+        theme="dark"
+        toastStyle={{
+          backgroundColor: "#555",
+          color: "#fff",
+        }}
+      />
+    </div>
+  );
 }
 
 export default App;
--- web/src/api.ts
+++ web/src/api.ts
@@ -1,90 +1,87 @@
 import type { Note } from "./types";
+import { toast } from "react-toastify";
 
 const API_URL = "http://localhost:3005/api";
 
 export async function getAllNotes(): Promise<Note[]> {
-	try {
-		const response = await fetch(`${API_URL}/notes`);
-		if (!response.ok) {
-			console.error("getAllNotes: server error");
-			return [];
-		}
-		const data = (await response.json()) as Note[];
-		return data;
-	} catch (error) {
-		if (error instanceof Error) {
-			console.log(error.message);
-		} else {
-			console.log("Unknown error", error);
-		}
-		return [];
-	}
+  try {
+    const response = await fetch(`${API_URL}/notes`);
+    if (!response.ok) {
+      throw new Error("Couldn't load notes");
+    }
+    const data = (await response.json()) as Note[];
+    return data;
+  } catch (error) {
+    if (error instanceof Error) {
+      toast.error("Error: " + error.message);
+    } else {
+      toast.error("Unknown error occured");
+    }
+    return [];
+  }
 }
 
 export async function updateNote(id: string, payload: any) {
-	try {
-		const response = await fetch(`${API_URL}/notes/${id}`, {
-			method: "PUT",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(payload),
-		});
-		if (!response.ok) {
-			console.error("updateNote: server error");
-			return;
-		}
-	} catch (error) {
-		if (error instanceof Error) {
-			console.log(error.message);
-		} else {
-			console.log("Unknown error", error);
-		}
-	}
+  try {
+    const response = await fetch(`${API_URL}/notes/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
+    if (!response.ok) {
+      throw new Error("Couldn't update the note");
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      toast.error("Error: " + error.message);
+    } else {
+      toast.error("Unknown error occured");
+    }
+  }
 }
 
 export async function deleteNote(id: string) {
-	try {
-		const response = await fetch(`${API_URL}/notes/${id}`, {
-			method: "DELETE",
-		});
-		if (!response.ok) {
-			console.error("server error");
-			return;
-		}
-	} catch (error) {
-		if (error instanceof Error) {
-			console.log(error.message);
-		} else {
-			console.log("Unknown error", error);
-		}
-	}
+  try {
+    const response = await fetch(`${API_URL}/notes/${id}`, {
+      method: "DELETE",
+    });
+    if (!response.ok) {
+      throw new Error("Couldn't delete note");
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      toast.error("Error: " + error.message);
+    } else {
+      toast.error("Unknown error occured");
+    }
+  }
 }
 
 export async function addNote(payload: {
-	position: string;
-	colors: string;
+  position: string;
+  colors: string;
 }): Promise<Note | null> {
-	try {
-		const response = await fetch(`${API_URL}/notes`, {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(payload),
-		});
-		if (!response.ok) {
-			console.error("server error", response);
-			return null;
-		}
-		const data = (await response.json()) as Note;
-		return data;
-	} catch (error) {
-		if (error instanceof Error) {
-			console.log(error.message);
-		} else {
-			console.log("Unknown error", error);
-		}
-		return null;
-	}
+  try {
+    const response = await fetch(`${API_URL}/notes`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
+    if (!response.ok) {
+      throw new Error("Couldn't add note");
+    }
+    const data = (await response.json()) as Note;
+    return data;
+  } catch (error) {
+    if (error instanceof Error) {
+      toast.error("Error: " + error.message);
+    } else {
+      toast.error("Unknown error occured");
+    }
+    return null;
+  }
 }
